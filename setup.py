3c74bfd5815c36a48ac9d4298866737f0af4cd6b
Nick Hurley <hurley@mozilla.com>
2011-03-28 10:27:22 -0700

    Distutils setup script

---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+setup(name='pgl', version='0.1', py_modules=['pgl'],
+    description='Python Git Extension Library', author='Nick Hurley',
+    author_email='hurley@todesschaf.org',
+    url='https://github.com/todesschaf/pgl')
